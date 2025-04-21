a4db92cd47f2244ff8bf5855accbe8618072fd27
wolvemax <lucasjoluizforte@gmail.com>
2025-04-21 02:55:40 -0300

    Update modelo.py

--- modelo.py
+++ modelo.py
@@ -188,6 +188,7 @@ ctx = webrtc_streamer(
     key="audio_stream_recorder",
     mode="sendonly",
     audio_receiver_size=256,
+    audio_processor_factory=AudioProcessor,
     media_stream_constraints={"audio": True, "video": False},
     rtc_configuration={
         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
